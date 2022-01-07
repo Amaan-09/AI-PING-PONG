ac75ddc4b7dd83aac5644f78a25b1578243e53ce
Amaan-09 <79093439+Amaan-09@users.noreply.github.com>
2022-01-07 14:16:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,6 +21,10 @@ var ball = {
     dy:3
 }
 
+rightWristY = 0;
+rightWristX = 0;
+scoreRightWrist = 0;
+
 function setup(){
   var canvas =  createCanvas(700,600);
   canvas.parent('canvas');
@@ -33,6 +37,22 @@ poseNet = ml5.poseNet(video, modelLoaded);
 poseNet.on('pose', gotPoses);
 }
 
+function modelLoaded()
+{
+  console.log("model loaded!");
+}
+
+gotPoses(results)
+{
+  if(results.length > 0)
+  {
+
+    rightWristY = results[0].pose.rightWrist.y;
+    rightWristX = results[0].pose.rightWrist.x;
+    scoreRightWrist =  results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
+  }
+}
 
 function draw(){
 
@@ -47,6 +67,13 @@ function draw(){
  fill("black");
  stroke("black");
  rect(0,0,20,700);
+ 
+ if(scoreRightWrist > 0.2)
+  {
+    fill("red");
+    stroke("red");
+    circle(rightWristX, rightWristY, 30);
+  }
  
    //funtion paddleInCanvas call 
    paddleInCanvas();
